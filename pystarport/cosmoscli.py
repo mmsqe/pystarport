ee0fbb1b75844da027302420eb7b79ea7b3e3207
mmsqe <mavis@crypto.com>
2024-07-23 18:46:56 +0800

    debug

--- pystarport/cosmoscli.py
+++ pystarport/cosmoscli.py
@@ -71,7 +71,9 @@ class ChainCommand:
     def __call__(self, cmd, *args, stdin=None, stderr=subprocess.STDOUT, **kwargs):
         "execute chain-maind"
         args = " ".join(build_cli_args_safe(cmd, *args, **kwargs))
-        return interact(f"{self.cmd} {args}", input=stdin, stderr=stderr)
+        cli = f"{self.cmd} {args}"
+        print("mm-cli", cli)
+        return interact(cli, input=stdin, stderr=stderr)
 
 
 class CosmosCLI:
